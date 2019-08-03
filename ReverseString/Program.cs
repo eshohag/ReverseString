197b0e8dcd00c0aa18831201237a61d8115d3266
Md. Shohag Mia <shohag.mia@datasoft-bd.com>
2019-08-03 17:04:30 +0600

    Optimized

--- ReverseString/Program.cs
+++ ReverseString/Program.cs
@@ -7,53 +7,45 @@ namespace ReverseString
         static void Main(string[] args)
         {
             Console.WriteLine("Enter any string:");
-            string str = Console.ReadLine();
+            var input = Console.ReadLine();
             string reverse = string.Empty;
 
-            if (str != null)
+            if (input != null)
+            {
+                var str = input.ToCharArray();
+
                 foreach (char ch in str)
                 {
                     reverse = ch + reverse;
                 }
-            Console.WriteLine("1st Reverse string is- " + reverse);
+                Console.WriteLine("1st Reverse string is- " + reverse);
 
-            reverse = string.Empty;
-            if (str != null)
+                reverse = string.Empty;
                 for (int i = str.Length - 1; i >= 0; i--)
                     reverse += str[i];
+                Console.WriteLine("2nd Reverse string is- {0}", reverse);
 
-            Console.WriteLine("2nd Reverse string is- {0}", reverse);
-
-            if (str != null)
-            {
-                char[] cArray = str.ToCharArray();
                 reverse = string.Empty;
-                for (int i = cArray.Length - 1; i >= 0; i--)
+                for (int i = str.Length - 1; i >= 0; i--)
                 {
-                    reverse += cArray[i];
+                    reverse += str[i];
                 }
                 Console.WriteLine($"3rd Reverse string is- {reverse}");
-            }
 
-            if (str != null)
-            {
                 int inc = str.Length - 1;
                 reverse = string.Empty;
                 while (inc >= 0)
                 {
-                    reverse += str.ToCharArray()[inc];
+                    reverse += str[inc];
                     inc--;
                 }
                 Console.WriteLine($"4th Reverse string is- {reverse}");
-            }
 
-            reverse = string.Empty;
-            if (str != null)
-            {
+                reverse = string.Empty;
                 int d = str.Length - 1;
                 do
                 {
-                    reverse += str.ToCharArray()[d];
+                    reverse += str[d];
                     d--;
                 } while (d >= 0);
                 Console.WriteLine($"5th Reverse string is- {reverse}");
